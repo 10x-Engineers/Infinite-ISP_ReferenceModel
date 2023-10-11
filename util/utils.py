49b2a222d7d39a3245a5647dcfb1ec02b0b15ad4
Maria_Nadeem-10x <maria.nadeem@10xengineers.ai>
2023-10-11 12:38:06 +0500

    added demosaic function instead of using zeros while saving raw img. replaced raw_to_rgb function with apply_cfa in utils.py

--- util/utils.py
+++ util/utils.py
@@ -299,104 +299,185 @@ def save_output_array(
 
         # for 1-channel raw: convert raw image to 8-bit rgb image
         if len(output_array.shape) == 2:
-            output_array = raw_to_rgb(output_array, bitdepth, bayer_pattern)
+            output_array = apply_cfa(output_array, 8, bayer_pattern)
         # save Image as .png
         plt.imsave(filename + ".png", output_array)
 
 
-def raw_to_rgb(raw_img, bitdepth, bayer_pattern):
-    """create a 3-channel RGB image from a 2d raw image
-    by appending zeros at unknown pixel values."""
-    shift_by = bitdepth - 8
-    raw_img = (raw_img >> shift_by).astype("uint8")
-    if bayer_pattern == "grbg":
-        # gr
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 0::2] = 1
-        gr_channel = mask * raw_img
+def masks_cfa_bayer(img, bayer):
+    """
+    Generating masks for the given bayer pattern
+    """
 
-        # r
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 1::2] = 1
-        r_channel = mask * raw_img
+    # dict will be creating 3 channel boolean type array of given shape with the name
+    # tag like 'r_channel': [False False ....] , 'g_channel': [False False ....] ,
+    # 'b_channel': [False False ....]
+    channels = dict(
+        (channel, np.zeros(img.shape, dtype=bool)) for channel in "rgb"
+    )
 
-        # b
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 0::2] = 1
-        b_channel = mask * raw_img
+    # Following comment will create boolean masks for each channel r_channel,
+    # g_channel and b_channel
+    for channel, (y_channel, x_channel) in zip(
+        bayer, [(0, 0), (0, 1), (1, 0), (1, 1)]
+    ):
+        channels[channel][y_channel::2, x_channel::2] = True
 
-        # gb
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 1::2] = 1
-        gb_channel = mask * raw_img
+    # tuple will return 3 channel boolean pattern for r_channel,
+    # g_channel and b_channel with True at corresponding value
+    # For example in rggb pattern, the r_channel mask would then be
+    # [ [ True, False, True, False], [ False, False, False, False]]
+    return tuple(channels[c] for c in "rgb")
 
-    if bayer_pattern == "rggb":
-        # r
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 0::2] = 1
-        r_channel = mask * raw_img
+def apply_cfa(img, bit_depth, bayer):
+    """
+    Demosaicing the given raw image
+    """
+    # 3D masks accoridng to the given bayer
+    mask_r, mask_g, mask_b = masks_cfa_bayer(img, bayer)
+    raw_in = np.float32(img)
 
-        # gr
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 1::2] = 1
-        gr_channel = mask * raw_img
+    # Declaring 3D Demosaiced image
+    demos_out = np.empty((raw_in.shape[0], raw_in.shape[1], 3))
 
-        # gb
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 0::2] = 1
-        gb_channel = mask * raw_img
+    # 5x5 2D Filter coefficients for linear interpolation of
+    # r_channel,g_channel and b_channel channels
+    # These filters helps to retain corresponding pixels information using
+    # laplacian while interpolation
 
-        # b
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 1::2] = 1
-        b_channel = mask * raw_img
+    # g_channel at r_channel & b_channel location,
+    g_at_r_and_b = (
+        np.float32(
+            [
+                [0, 0, -1, 0, 0],
+                [0, 0, 2, 0, 0],
+                [-1, 2, 4, 2, -1],
+                [0, 0, 2, 0, 0],
+                [0, 0, -1, 0, 0],
+            ]
+        )
+        * 0.125
+    )
 
-    if bayer_pattern == "bggr":
-        # b
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 0::2] = 1
-        b_channel = mask * raw_img
+    # r_channel at green in r_channel row & b_channel column --
+    # b_channel at green in b_channel row & r_channel column
+    r_at_gr_and_b_at_gb = (
+        np.float32(
+            [
+                [0, 0, 0.5, 0, 0],
+                [0, -1, 0, -1, 0],
+                [-1, 4, 5, 4, -1],
+                [0, -1, 0, -1, 0],
+                [0, 0, 0.5, 0, 0],
+            ]
+        )
+        * 0.125
+    )
 
-        # gb
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 1::2] = 1
-        gb_channel = mask * raw_img
+    # r_channel at green in b_channel row & r_channel column --
+    # b_channel at green in r_channel row & b_channel column
+    r_at_gb_and_b_at_gr = np.transpose(r_at_gr_and_b_at_gb)
 
-        # gr
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 0::2] = 1
-        gr_channel = mask * raw_img
+    # r_channel at blue in b_channel row & b_channel column --
+    # b_channel at red in r_channel row & r_channel column
+    r_at_b_and_b_at_r = (
+        np.float32(
+            [
+                [0, 0, -1.5, 0, 0],
+                [0, 2, 0, 2, 0],
+                [-1.5, 0, 6, 0, -1.5],
+                [0, 2, 0, 2, 0],
+                [0, 0, -1.5, 0, 0],
+            ]
+        )
+        * 0.125
+    )
 
-        # r
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 1::2] = 1
-        r_channel = mask * raw_img
+    # Creating r_channel, g_channel & b_channel channels from raw_in
+    r_channel = raw_in * mask_r
+    g_channel = raw_in * mask_g
+    b_channel = raw_in * mask_b
 
-    if bayer_pattern == "gbrg":
-        # gb
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 0::2] = 1
-        gb_channel = mask * raw_img
+    # Creating g_channel channel first after applying g_at_r_and_b filter
+    g_channel = np.where(
+        np.logical_or(mask_r == 1, mask_b == 1),
+        correlate2d(raw_in, g_at_r_and_b, mode="same", boundary="symm"),
+        g_channel,
+    )
 
-        # b
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[0::2, 1::2] = 1
-        b_channel = mask * raw_img
+    # Applying other linear filters
+    rb_at_g_rbbr = correlate2d(
+        raw_in, r_at_gr_and_b_at_gb, mode="same", boundary="symm"
+    )
+    rb_at_g_brrb = correlate2d(
+        raw_in, r_at_gb_and_b_at_gr, mode="same", boundary="symm"
+    )
+    rb_at_gr_bbrr = correlate2d(
+        raw_in, r_at_b_and_b_at_r, mode="same", boundary="symm"
+    )
 
-        # r
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 0::2] = 1
-        r_channel = mask * raw_img
+    # After convolving the input raw image with rest of the filters,
+    # now we have the respective interpolated data, now we just have
+    # to extract the updated pixels values according to the
+    # position they are meant to be updated
 
-        # gr
-        mask = np.zeros(raw_img.shape, dtype="uint8")
-        mask[1::2, 1::2] = 1
-        gr_channel = mask * raw_img
+    # Extracting Red rows.
+    r_rows = np.transpose(np.any(mask_r == 1, axis=1)[np.newaxis]) * np.ones(
+        r_channel.shape, dtype=np.float32
+    )
 
-    g_channel = gr_channel + gb_channel
-    rgb_array = np.stack([r_channel, g_channel, b_channel], axis=2)
+    # Extracting Red columns.
+    r_col = np.any(mask_r == 1, axis=0)[np.newaxis] * np.ones(
+        r_channel.shape, dtype=np.float32
+    )
 
-    return rgb_array
+    # Extracting Blue rows.
+    b_rows = np.transpose(np.any(mask_b == 1, axis=1)[np.newaxis]) * np.ones(
+        b_channel.shape, dtype=np.float32
+    )
+
+    # Extracting Blue columns
+    b_col = np.any(mask_b == 1, axis=0)[np.newaxis] * np.ones(
+        b_channel.shape, dtype=np.float32
+    )
+
+    # For R channel we have to update pixels at [r_channel rows
+    # and b_channel cols] & at [b_channel rows and r_channel cols]
+    # 3 pixels need to be updated near one given r_channel
+    r_channel = np.where(
+        np.logical_and(r_rows == 1, b_col == 1), rb_at_g_rbbr, r_channel
+    )
+    r_channel = np.where(
+        np.logical_and(b_rows == 1, r_col == 1), rb_at_g_brrb, r_channel
+    )
+
+    # Similarly for B channel we have to update pixels at
+    # [r_channel rows and b_channel cols]
+    # & at [b_channel rows and r_channel cols] 3 pixels need
+    # to be updated near one given b_channel
+    b_channel = np.where(
+        np.logical_and(b_rows == 1, r_col == 1), rb_at_g_rbbr, b_channel
+    )
+    b_channel = np.where(
+        np.logical_and(r_rows == 1, b_col == 1), rb_at_g_brrb, b_channel
+    )
+
+    # Final r_channel & b_channel channels
+    r_channel = np.where(
+        np.logical_and(b_rows == 1, b_col == 1), rb_at_gr_bbrr, r_channel
+    )
+    b_channel = np.where(
+        np.logical_and(r_rows == 1, r_col == 1), rb_at_gr_bbrr, b_channel
+    )
+
+    demos_out[:, :, 0] = r_channel
+    demos_out[:, :, 1] = g_channel
+    demos_out[:, :, 2] = b_channel
+
+    # Clipping the pixels values within the bit range
+    demos_out = np.clip(demos_out, 0, 2**bit_depth - 1)
+    demos_out = np.uint8(demos_out)
+    return demos_out
 
 
 def save_output_array_yuv(img_name, output_array, module_name, platform, conv_std):
