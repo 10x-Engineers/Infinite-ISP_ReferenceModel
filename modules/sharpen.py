e51e44a12acafc7ea17830b9ec78b9916ecc4d41
sohaibimran-10xe <sohaib.imran@10xengineers.ai>
2025-01-08 19:58:35 +0500

    float point implementation of sharpening

--- modules/sharpen.py
+++ modules/sharpen.py
@@ -52,4 +52,29 @@ class Sharpening:
         return self.img
 
     def apply_sharpen(self):
+        """
+        Applying sharpening to the input image
+        """
+        self.sharpen_sigma = self.parm_sha["sharpen_sigma"]
+        self.sharpen_strength = self.parm_sha["sharpen_strength"]
+
+
+        luma = np.float32(self.img[:, :, 0])
+
+        # if self.img.dtype == "float32":
+        #     luma = np.round(255 * luma).astype(np.uint8)
+
+        # Filter the luma component of the image with a Gaussian LPF
+        # Smoothing magnitude can be controlled with the sharpen_sigma parameter
+        smoothened = ndimage.gaussian_filter(luma, self.sharpen_sigma)
+        # Sharpen the image with upsharp mask
+        # Strength is tuneable with the sharpen_strength parameter
+        sharpened = luma + ((luma - smoothened) * self.sharpen_strength)
+
+        if self.img.dtype == "float32":
+            # sharpened = np.float32((sharpened) / 255.0)
+            # out = sharpened.astype("float32")
+            self.img[:, :, 0] = np.clip(sharpened, 0, 1)
+        else:
+            self.img[:, :, 0] = np.uint8(np.clip(sharpened, 0, 255))
         return self.img 
